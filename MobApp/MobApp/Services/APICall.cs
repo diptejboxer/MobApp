4b97aaeb2ba4c79e7e6d8db938baf500d756b0d4
diptejboxer <diptej.thakkar@boxerproperty.com>
2019-06-24 22:58:21 +0530

    Add project files.

---
+++ MobApp/MobApp/Services/APICall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MobApp.Services
+{
+   public class APICall
+    {
+        public async void login(string email, string password)
+        {
+            var client = new HttpClient();
+            var response = await client.GetStringAsync("");
+        }
+    }
+}
